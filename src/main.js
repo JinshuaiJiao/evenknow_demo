fc8ad24504a3b23878ba8c0ab3858657234c832a
jiaojinshuai <jiaojinshuai@zuirenli.com>
2025-04-25 15:17:39 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,14 +3,10 @@ import App from "./App.vue";
 import ElementPlus from "element-plus";
 import JinsButton from "jins-button";
 import "jins-button/jins-button.css";
-// import NormalTags from "normal-tags";
-// import "normal-tags/normal-tags.css";
 import "element-plus/dist/index.css";
 
-// console.log(NormalTags);
 const app = createApp(App);
 app.use(JinsButton);
-// app.use(NormalTags);
 app.use(ElementPlus);
 
 app.mount("#app");
