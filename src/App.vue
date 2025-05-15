86b35f3f53f398a33cf73aed96d27f3dc096cf79
jiaojinshuai <jiaojinshuai@zuirenli.com>
2025-05-15 17:47:46 +0800

    Feat: 嵌套飞书页面

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,9 @@
 <template>
-  <div id="app">
-    <demo />
-    
-    <!-- <normal-tags label="我的标签" placement="bottom" content="666" type="success" /> -->
-    <jins-button />
-
-    <ajj style="margin-top: 30px;" />
-
-    <image-cropper/>
-    <vue-office-docx
-        :src="docx"
-        style="height: 100vh;"
-    />
-  </div>
+    <iframe src="https://hvs5y0ntl4p.feishu.cn/wiki/EIA4wK7LpiqhNqkgE6ecIWb6ncd" frameborder="0"></iframe>
 </template>
-<script setup>
-import { defineComponent, ref } from 'vue';
-import Ajj from './ajj.vue'
-import ImageCropper from './image-cropper.vue';
-import Demo from './pages/demo.vue';
-
-//引入VueOfficeDocx组件
-import VueOfficeDocx from '@vue-office/docx'
-//引入相关样式
-import '@vue-office/docx/lib/index.css'
-
-defineComponent([VueOfficeDocx])
-
-const docx = ref('http://static.shanhuxueyuan.com/test6.docx');
-</script>
-
-<style>
-#app {
-  margin-top: 60px;
+<style scoped>
+iframe {
+  width: 100%;
+  height: 100%;
 }
 </style>
